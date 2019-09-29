a28c4f0e707c598cd6ee513d7fb46c6999bae3c9
无情小黄人 <lipei729744115@163.com>
2019-09-29 15:55:43 +0800

    搜索结果加载事件完成+返回搜索界面

--- src/views/results/index.vue
+++ src/views/results/index.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="results">
-    <van-nav-bar title="搜索结果" left-arrow />
+    <van-nav-bar title="搜索结果" left-arrow @click-left="onClickLeft"/>
     <van-cell-group>
-      <van-cell v-for="item in resultList" :title="item.title" >
-        <template slot="label">
-          <p>{{item.aut_name}}</p>
-          <van-grid :column-num="3">
-            <van-grid-item text="评论" />
-            <van-grid-item text="点赞" />
-            <van-grid-item text="收藏" />
-          </van-grid>
-        </template>
-      </van-cell>
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <van-cell v-for="item in resultList" :title="item.title">
+          <template slot="label">
+            <p>{{item.aut_name}}</p>
+            <van-grid :column-num="3">
+              <van-grid-item text="评论" />
+              <van-grid-item text="点赞" />
+              <van-grid-item text="收藏" />
+            </van-grid>
+          </template>
+        </van-cell>
+      </van-list>
     </van-cell-group>
   </div>
 </template>
@@ -22,20 +24,40 @@ export default {
   name: "results",
   data() {
     return {
+      loading: false,
+      finished: false,
       page: 1,
-      per_page: 10,
+      per_page: 50,
       resultList: []
     };
   },
-  async created() {
-    //   点击了历史搜索
-    let data = {
-      page: this.page,
-      per_page: this.per_page,
-      q: this.$route.params.key
-    };
-    let res = await toSearch(data);
-    this.resultList = res.data.data.results;
+  methods: {
+    //   返回
+    onClickLeft(){
+    this.$router.push('/search');
+    },
+
+    async onLoad() {
+      //   点击了历史搜索
+
+      let data = {
+        page: this.page,
+        per_page: this.per_page,
+        q: this.$route.params.key
+      };
+      let res = await toSearch(data);
+      this.page++;
+        console.log(res);
+      this.resultList.push(...res.data.data.results);
+      this.loading = false;
+      console.log(res.data.data.total_count);
+      
+      if (Math.ceil(res.data.data.total_count / this.per_page) < this.page) {
+        console.log(11);
+
+        this.finished = true;
+      }
+    }
   }
 };
 </script>
@@ -46,10 +68,11 @@ export default {
   .van-icon-arrow-left {
     color: white;
   }
-  .van-cell-group{
-      background-color: #f0f3f6;
-  .van-cell{
+  .van-cell-group {
+    background-color: #f0f3f6;
+    .van-cell {
       margin-bottom: 10px;
-  }}
+    }
+  }
 }
 </style>
